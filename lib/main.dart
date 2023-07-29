ee8c7f3b382234fc6fbdc7bd10a241cba173f029
robertomscjp <robertomscjp@gmail.com>
2023-07-28 22:18:26 -0300

    HomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
+import 'dart:js';
+
+import 'package:app_vendas/src/componetes/pages/HomePage.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final routes = <String, WidgetBuilder>{
+    Homepage.tag: (context) => Homepage(),
+  };
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Aplicativo de Vendas',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Aplicativo de Vendas'),
-        ),
-        body: Center(
-          child: Text('Bem vindo(a)'),
-        ),
-      ),
+      home: Homepage(),
+      routes: routes,
     );
   }
 }
